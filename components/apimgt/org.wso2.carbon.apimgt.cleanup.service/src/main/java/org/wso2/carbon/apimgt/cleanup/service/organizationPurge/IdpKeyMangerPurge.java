f8e5b0be7ee2cda32acf1dcf165bdbe8bcb71f92
hiranya <hiranyakavi@gmail.com>
2021-07-19 11:15:27 +0530

    Adding interface to org deletion feature

---
+++ components/apimgt/org.wso2.carbon.apimgt.cleanup.service/src/main/java/org/wso2/carbon/apimgt/cleanup/service/organizationPurge/IdpKeyMangerPurge.java
@@ -0,0 +1,9 @@
+package org.wso2.carbon.apimgt.cleanup.service.organizationPurge;
+
+import javax.ws.rs.core.Response;
+
+public class IdpKeyMangerPurge implements OrganizationPurge {
+    public Response deleteOrganization(String orgId) {
+        return null;
+    }
+}
