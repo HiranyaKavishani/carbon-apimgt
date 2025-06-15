6b4fe80354e4735abbd2686c568d3e9cdf3bf167
Tharindu Dharmarathna <tharindua@wso2.com>
2025-06-15 23:18:30 +0530

    add Model family support

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/PublisherCommonUtils.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/PublisherCommonUtils.java
@@ -2067,6 +2067,7 @@ public class PublisherCommonUtils {
             throw new APIManagementException("Invalid/Malformed endpoint URL(s) detected",
                     ExceptionCodes.INVALID_ENDPOINT_URL);
         }
+        APIUtil.validateAPIEndpointConfig(apiDto.getEndpointConfig(), apiDto.getType().toString(), apiDto.getName());
 
         // validate gateway type before proceeding
         String gatewayType = apiDto.getGatewayType();
