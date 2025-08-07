3a43efb8354472ecaf3a1e5570397a54f7d55993
BiyonFernando <biyon@wso2.com>
2025-08-07 16:03:01 +0530

    Publisher portal API to get deployment status

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/APIMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/APIMappingUtil.java
@@ -4710,6 +4710,10 @@ public class APIMappingUtil {
                         + model.getSuccessDeployedTime(), e);
             }
         }
+        apiRevisionDeploymentDTO.setLiveGatewayCount(model.getLiveGatewayCount());
+        apiRevisionDeploymentDTO.setDeployedGatewayCount(model.getDeployedGatewayCount());
+        apiRevisionDeploymentDTO.setFailedGatewayCount(model.getFailedGatewayCount());
+
         return apiRevisionDeploymentDTO;
     }
 
