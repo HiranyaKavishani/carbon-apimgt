541595231e4a81ac4ec0e60f74141b961e82484d
BiyonFernando <biyon@wso2.com>
2025-08-10 22:31:12 +0530

    Address suggestions in code review 2

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -5769,7 +5769,7 @@ public final class APIUtil {
         }
     }
 
-    private static InetAddress getLocalAddress() {
+    public static InetAddress getLocalAddress() {
 
         Enumeration<NetworkInterface> ifaces = null;
         try {
